245a25821b706fa99383b62763bddb39b1c5d2d4
Philippe FOUQUET <p.fouquet@environnement-sa.com>
2013-10-17 08:39:29 +0200

    Initial Commit

---
+++ NoteBinder/RADocument.h
@@ -0,0 +1,13 @@
+//
+//  RADocument.h
+//  NoteBinder
+//
+//  Created by Philippe Fouquet on 17/10/13.
+//  Copyright (c) 2013 Philippe Fouquet. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RADocument : NSPersistentDocument
+
+@end
---
+++ NoteBinder/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  NoteBinder
+//
+//  Created by Philippe Fouquet on 17/10/13.
+//  Copyright (c) 2013 Philippe Fouquet. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
